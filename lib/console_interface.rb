ade1f88c6aad75d38a17edde6c7a79e3de855a10
Rustam Shaidullov <r.91@mail.ru>
2020-10-16 16:29:34 +0300

    Update console_interface.rb

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,8 +1,8 @@
 class ConsoleInterface
   FIGURES =
     Dir[__dir__ + '/../data/figures/*.txt'].
-    sort.
-    map { |file_name| File.read(file_name) }
+      sort.
+      map { |file_name| File.read(file_name) }
 
   def initialize(game)
     @game = game
